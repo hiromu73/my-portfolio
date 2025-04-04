77748df5c0c0ad0184949cb53baf515660f618ae
hiromu73 <hiromu.camera@gmail.com>
2025-04-04 23:46:45 +0900

    更新：ui更新

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,12 +1,28 @@
-
-
-
-import React from 'react'
+import React from "react";
 
 const Profile = () => {
   return (
-    <div className=''>Profile</div>
-  )
-}
+    <div className="text-center w-5/6">
+      <h2 className="font-bold text-2xl mb-5">Profile</h2>
+      <div className="h-full bg-green-100 rounded-xl pt-3">
+        <ul className=" mt-5 pb-5">
+          <li className="mb-3">4年生大学を卒業後、広告代理店等の営業職を6年間勤務</li>
+          <li className="mb-3">28歳の時に友人の影響を受け、独学の地にSESに転職</li>
+          <li className="mb-3">32歳現在、社員200名程の受託会社に勤務</li>
+        </ul>
+        <h5>Technology Stacks</h5>
+        <ul className=" mt-5 pb-5 ">
+          <li className="mb-3">frontend: Javascript・Typescript・Next.js(React), </li>
+          <li className="mb-3">backend: C#・Python(FastAPI)</li>
+        </ul>
+        <h5>Personal Development</h5>
+        <ul className=" mt-5 pb-5 ">
+          <li className="mb-3">・Flutter(Dart)にてモバイルアプリ開発 </li>
+          <li className="mb-3">・Python(Langchain), Difyによる生成AI </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
--- src/components/TopPage.tsx
+++ src/components/TopPage.tsx
@@ -3,18 +3,14 @@ import React from "react";
 
 const TopPage = () => {
   return (
-    <div className="w-5/6 bg-sky-100 rounded-xl">
+    <div className="w-5/6 h-full bg-sky-100 rounded-xl">
       <div className="flex items-center space-x-4 p-4 gap-10">
-        <div className="rounded-full bg-white w-1/2 h-1/2">
-          <Image
-            src="/next.svg"
-            alt="Profile"
-            width={200} // 必須: 画像の幅を指定
-            height={200} // 必須: 画像の高さを指定
-          />
+        <div className="rounded-full">
+          <Image src="/public/my_profile.jpg" alt="Profile" width={500} height={500} className="rounded-full" />
         </div>
 
         <div className="space-y-2 ">
+          <h1 className="font-bold text-2xl">HIROMU TOJIMA</h1>
           <ul>
             <li>サウナが好き</li>
             <li>開発が好き</li>
